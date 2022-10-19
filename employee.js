32c0fb5610fbe04a787cb6cbcd0b06acabc8a08d
Keenly <keenlyhere@gmail.com>
2022-10-18 18:39:23 -0700

    first commit

---
+++ employee.js
@@ -0,0 +1,15 @@
+class Employee {
+    constructor(name, salary, title, manager) {
+        this.name = name;
+        this.salary = salary;
+        this.title = title;
+        this.manager = null;
+    }
+    
+}
+
+
+// const leo = new Employee('Leonardo', 90000, 'Ninja');
+// console.log(leo)
+
+module.exports = Employee;
